019a23b116f960281e0c073d4b5b16270dd669b6
zizy-bilal <bilal@zizywin.com>
2023-07-30 04:36:10 +0300

    Removed unused erc20pausable implementation

--- contracts/tokens/ERC20Def.sol
+++ contracts/tokens/ERC20Def.sol
@@ -4,13 +4,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // @dev Default ERC20 Token
 contract ERC20Def is Context, ERC20, ERC20Burnable, Ownable {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
     uint8 constant DECIMALS = 8;
 
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
